003fb85d872455cb9ed445039167c2d9c4103754
StVrAleks <orchid86@yandex.ru>
2025-05-04 19:25:00 +0300

    Update app.js

--- control1/app.js
+++ control1/app.js
@@ -1,3 +1,4 @@
+
 const http = require("http");
 const fs = require("fs");
 const express = require("express"); // получаем модуль express
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const path = require('path');
 const { forEach } = require("lodash");
+const { error } = require("console");
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/page", function (_, response) {
@@ -155,33 +157,35 @@ app.options('/run',(request, response)=>{
   response.send("");
 }
 );
-    app.post("/run", jsonParser, cors(CORSOptions), async function (request, response) {
+app.post("/run", jsonParser, cors(CORSOptions), async function (request, response) {
+  let resItem = '';
       if(!request.body) return response.sendStatus(400);
-      response.setHeader('Access-Control-Allow-Origin', '*');
-     
+    try{      
+  //    response.setHeader('Access-Control-Allow-Origin', '*');
+ 
       let reqItems = request.body.allItemsForm;
-   //   console.log("reqItems",reqItems, reqItems);
       let reqMethod = JSON.stringify(reqItems["item1"]);
       let reqUrl = JSON.stringify(reqItems['item2']);
       let reqParam0 = reqItems['item3'];
-    //  console.log("reqParam0",JSON.stringify(reqParam0));
       let reqParam = Object.assign({}, reqParam0);
       let reqBody = JSON.stringify(reqItems['item5']);
       let reqHead0 = JSON.stringify(reqItems['item6']);
       let reqHead = Object.assign({}, reqHead0);
-      console.log(reqMethod);
-      let resItem;
-
+      console.log(reqMethod,reqParam );
+      console.log('items', reqItems);
+      
       if(reqMethod === '"GET"')
       {
-        //console.log('v get');
+        console.log('v get');
         if(!reqParam)
         {
           for(key in reqParam)
             var strParam = strParam + key + '=' + reqParam[key] + '&';
           reqUrl= reqUrl + '?'+ strParam;
         }  
-        resItem = await getReq(reqUrl, reqMethod, reqHead);      
+        console.log('reqParam', reqUrl, reqMethod, reqHead);
+        resItem = await getReq(reqUrl, reqMethod, reqHead); 
+        console.log('resItem', resItem);   
       }
       else if(reqMethod === '"POST"')
       {
@@ -197,32 +201,17 @@ app.options('/run',(request, response)=>{
         {
           resItem = await postReq(reqUrl, reqMethod, reqHead, reqBody);
           console.log("resItem!!!", resItem);  
-        }        
-     response.send(resItem);
+        }  
+      response.send(resItem);           
+      }    
+      catch{
+        console.log('error /RUN', error);
+      }
     });      
-        
-
-async function getReq(reqUrl, reqMethod, reqHead) {
-  try{
-      let res = await fetch(reqUrl.replaceAll('"', ""),{
-      method: reqMethod.replaceAll('"', ""),
-      redirect: 'manual',
-      headers: reqHead
-    });
-    let rez = await sendRes(res);
-  }
-   catch
-   {
-    console.log(' getReqerror');
-    return 0;
-   } 
-    return rez;
-    }         
-     
 
 //*******POST */
-    async function postReq(reqUrl, reqMethod, reqHead, reqBody) {
-      let resObj = {};
+async function postReq(reqUrl, reqMethod, reqHead, reqBody) {
+    let resObj = {};
     let h= new Array, b= new Array;
     var i =0;          
   try{
@@ -242,41 +231,41 @@ async function getReq(reqUrl, reqMethod, reqHead) {
         } 
     if(rT.includes('image'))   //если возвращается картинка
      {
-      const arrayBuffer = await res.arrayBuffer();
-      const base64data = Buffer.from(arrayBuffer).toString("base64");
-      const dataUrl = `data:${res.headers.get("content-type")};base64,${base64data}`
-      const pic = JSON.stringify(dataUrl, null, 2);
-      console.log(dataUrl);
+      let arrayBuffer = await res.arrayBuffer();
+      let base64data = Buffer.from(arrayBuffer).toString("base64");
+      let dataUrl = `data:${res.headers.get("content-type")};base64,${base64data}`
+      let pic = JSON.stringify(dataUrl, null, 2);
       resObj.body = JSON.parse(pic);
      }
      else //если возвращается не картинка
       resObj.body = await readBody(res.body, res.headers); 
-
       resObj.st = await res.status;
       resObj.hd = h;
       resObj.ok = await res.ok;
-     // console.log("resObj.hd",resObj.hd);
       return await resObj; 
   }
   catch{
-    console.log('postReq error');
-    return 0;
+    console.log('postReq error', error);
   }  
-
-    }
+  }
 
 
     //*******GET */
-  async function getReq(reqUrl, reqMethod, reqHead) {
-     let resObj = {};
+  async function getReq(freqUrl, freqMethod, freqHead) {
+    let resObj = {};
     let h= new Array, b= new Array;
     var i =0;     
+    let reqUrl = freqUrl.replaceAll('"', "");
+    let reqMethod = freqMethod.replaceAll('"', "");
+    let reqHead = freqHead;
   try{
-      let res = await fetch(reqUrl.replaceAll('"', ""),{
-      method: reqMethod.replaceAll('"', ""),
+     console.log('for fetch',reqUrl,reqMethod,reqHead);
+      let res = await fetch(reqUrl,{
+      method: reqMethod,
       redirect: 'manual',
       headers: reqHead
     });
+    console.log('posle fetch');
     for(header of res.headers)
           {
             h[i] = header[0];
@@ -285,14 +274,16 @@ async function getReq(reqUrl, reqMethod, reqHead) {
               var rT = h[i+1];
             i= i+2;
           } 
+      console.log('head', h);    
       if(rT.includes('image'))   //если возвращается картинка
        {
+        console.log('if img');
         const arrayBuffer = await res.arrayBuffer();
         const base64data = Buffer.from(arrayBuffer).toString("base64");
         const dataUrl = `data:${res.headers.get("content-type")};base64,${base64data}`
         const pic = JSON.stringify(dataUrl, null, 2);
-        console.log(dataUrl);
         resObj.body = JSON.parse(pic);
+        console.log('resObj.body', resObj.body);
        }
        else //если возвращается не картинка
         resObj.body = await readBody(res.body, res.headers); 
@@ -305,8 +296,7 @@ async function getReq(reqUrl, reqMethod, reqHead) {
   }
    catch
    {
-    console.log(' getReqerror');
-    return 0;
+    console.log(' getReqerror', error);
    } 
     
     } 
