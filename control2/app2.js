0accfabcc26af659e4c5e851eaf65fefc19d4f93
StVrAleks <orchid86@yandex.ru>
2025-05-13 20:44:44 +0300

    Update app2.js

--- control2/app2.js
+++ control2/app2.js
@@ -1,34 +1,60 @@
-
 const http = require("http");
 const fs = require("fs");
 const readline = require('readline-sync');
 
 let objFiles = {};
 let objArh = {};
-let i =0;
-fs.readdir(__dirname + "/compres", function(error,data) {
+/*fs.readdir(__dirname + "/compres", function(error,data) {
   if (error) throw error;
   
   console.log(data);
- 
-for(var i=0; i< data.length; i++)
+ */
+let allFiles = getFiles();
+/*fs.readdir(__dirname + "/compres", function(error,data) {
+  if (error) throw error;
+  console.log('ddd',data);  
+   allFiles = data;
+
+ //  return items; 
+  });*/
+console.log('aaa',allFiles);
+for(var i=0; i< allFiles.length; i++)
 {
-  var nStr = data[i];
-   fs.stat(__dirname + "/compres" + data[i]+'/tmp/', function(error,stat){
+  var nStr = allFiles[i];
+   fs.stat(__dirname + "/compres/" + allFiles[i], function(error,stat){
     if(error) {  // если возникла ошибка
         return console.log(error);
     }
-    console.log(stat);   // выводим считанные данные
-});
-  console.log(nStr);
-if(nStr.includes('gz'))
+    if(nStr.includes('gz'))
+     objArh[i] = {[allFiles[i]]:'', [stat.mtime]:''};
+    else
+    objFiles[i] = {[allFiles[i]]:'',  [stat.mtime]:''};
+  console.log(stat.mtime);   // выводим считанные данные 
+  });
+}
+   
+  
+ // console.log(nStr);
+/*if(nStr.includes('gz'))
      objArh[i] = {[data[i]]:''};
 else
     objFiles[i] = {[data[i]]:''};
-}
+}*/
 console.log('objFiles',objFiles); 
-});
+//});
 
+
+async function getFiles(){
+let items ='';
+ await fs.readdir(__dirname + "/compres", function(error,data) {
+  if (error) throw error;
+   items = data;
+  console.log('asda',data);
+  // return items; 
+  });
+  return await data;
+
+}
 /*fs.readFile(__dirname + "/compres", function(error,data){
     if(error) {  // если возникла ошибка
         return console.log(error);
