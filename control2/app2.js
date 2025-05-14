649cd23eddb63c8a4085c9f846e755f9506a080e
StVrAleks <orchid86@yandex.ru>
2025-05-14 17:01:16 +0300

    Update app2.js

--- control2/app2.js
+++ control2/app2.js
@@ -74,3 +74,63 @@ rl.question('What do you think of Node.js? ', (answer) => {
 
   rl.close();
 });*/
+
+const http = require("http");
+const fs = require("fs");
+//const readline = require('readline-sync');
+
+let objFiles = {};
+let objArh = {}; 
+
+letStart();
+function letStart(){
+let allItems, res;
+let a = getFiles();
+console.log("Информация по каждому файлу FFF", a);
+}
+
+
+async function getFiles(){
+ let items ='';
+ console.log('Я зашел в папку');
+ await fs.readdir(__dirname + "/compres", function(error,data) {
+    if (error) throw error; 
+    items = data;
+    console.log('В папке найдены файлы:', items);
+    let allFiles = getFilesData(items);
+    return allFiles;
+}) 
+
+
+}
+
+async function getFilesData(allItems){
+console.log('Hадо обработать следующие файлы', allItems);
+//for(var i=0; i< allItems.length; i++)
+//{
+var i=-1;
+{  
+  console.log(allItems.length);
+ var i=i+1; 
+ var nStr = allItems[i]; 
+ new Promise((resolve, reject) =>{
+
+  fs.stat(__dirname + "/compres/" + allItems[i], function(error,stat){
+    if(error) {  return console.log(error);  } // если возникла ошибка 
+      return resolve(stat.mtime);
+  })})
+  .then(resolve =>  {
+    //console.log("resolve", resolve);
+    if(nStr.includes('gz'))
+      objArh[i] = {[allItems[i]]:'', [resolve]:''};
+    else
+      objFiles[i] = {[allItems[i]]:'',  [resolve]:''};
+  console.log('выводим считанные данные', resolve);   // выводим считанные данные 
+  console.log(objArh, objFiles);
+return objFiles;
+});
+}while(i >= allItems.length);
+//}
+
+}
+
