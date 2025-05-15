31e25d96f53b2c871f4e9dd839980c105439944d
StVrAleks <orchid86@yandex.ru>
2025-05-15 18:50:07 +0300

    15_05

--- control2/app2.js
+++ control2/app2.js
@@ -1,136 +1,155 @@
-const http = require("http");
 const fs = require("fs");
+const zlib = require("zlib");
 const readline = require('readline-sync');
 
-let objFiles = {};
-let objArh = {};
-/*fs.readdir(__dirname + "/compres", function(error,data) {
-  if (error) throw error;
-  
-  console.log(data);
- */
-let allFiles = getFiles();
-/*fs.readdir(__dirname + "/compres", function(error,data) {
-  if (error) throw error;
-  console.log('ddd',data);  
-   allFiles = data;
-
- //  return items; 
-  });*/
-console.log('aaa',allFiles);
-for(var i=0; i< allFiles.length; i++)
-{
-  var nStr = allFiles[i];
-   fs.stat(__dirname + "/compres/" + allFiles[i], function(error,stat){
-    if(error) {  // если возникла ошибка
-        return console.log(error);
-    }
-    if(nStr.includes('gz'))
-     objArh[i] = {[allFiles[i]]:'', [stat.mtime]:''};
-    else
-    objFiles[i] = {[allFiles[i]]:'',  [stat.mtime]:''};
-  console.log(stat.mtime);   // выводим считанные данные 
-  });
-}
-   
-  
- // console.log(nStr);
-/*if(nStr.includes('gz'))
-     objArh[i] = {[data[i]]:''};
-else
-    objFiles[i] = {[data[i]]:''};
-}*/
-console.log('objFiles',objFiles); 
-//});
-
-
-async function getFiles(){
-let items ='';
- await fs.readdir(__dirname + "/compres", function(error,data) {
-  if (error) throw error;
-   items = data;
-  console.log('asda',data);
-  // return items; 
-  });
-  return await data;
-
-}
-/*fs.readFile(__dirname + "/compres", function(error,data){
-    if(error) {  // если возникла ошибка
-        return console.log(error);
-    }
-    console.log(data.toString());   // выводим считанные данные
-});*/
-
-/*
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-console.log('3');
-rl.question('What do you think of Node.js? ', (answer) => {
-  // TODO: Log the answer in a database
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
-  rl.close();
-});*/
-
-const http = require("http");
-const fs = require("fs");
-//const readline = require('readline-sync');
-
-let objFiles = {};
-let objArh = {}; 
-
+let filesInfo = {'file':{}, 'stat':{}, 'name':{}, 'format':{}};
+let path = '';
 letStart();
-function letStart(){
-let allItems, res;
-let a = getFiles();
-console.log("Информация по каждому файлу FFF", a);
+
+async function letStart(){
+//Укажите путь до папки для автокомпрессора, включая имя папки:
+const answer = readline.question('Specify the path to the folder for the autocompressor, including the folder name: ',{ encoding: 'utf-8' });//, (answer) => {
+
+  path = answer;
+console.log("path1", answer);  
+  let lastItem = answer.substr(answer.length - 1);
+  if(lastItem === '/' || lastItem === '/\/' || lastItem === '|')
+    path = answer.substr(0, answer.length - 1);
+ // readline.close();
+//});
+console.log("path", path);
+await getFiles();
 }
 
+function getFiles(){
+ console.log('Я зашел в папку', path);
+  //  fs.readdir(__dirname + "/compres", function(error,data) {
+    fs.readdir(path, function(error,data) {
+      if (error) throw error; 
+      console.log('В папке найдены файлы:', data);
+      for(var i=0; i<data.length; i++)
+        {
+         // var file = __dirname + "/compres/" + data[i];
+          var file = path + "/" + data[i];
+          filesInfo['file'][i] = data[i];
+          filesInfo['name'][i] = data[i].substring(0, data[i].indexOf("."));
+          filesInfo['format'][i] = data[i].slice(data[i].lastIndexOf(".") + 1);
+          console.log("Получили сведения о" + file);
+          fs.stat(file, generate_callback(file, i, data.length-1));
+        }
+    })
+}
 
-async function getFiles(){
- let items ='';
- console.log('Я зашел в папку');
- await fs.readdir(__dirname + "/compres", function(error,data) {
-    if (error) throw error; 
-    items = data;
-    console.log('В папке найдены файлы:', items);
-    let allFiles = getFilesData(items);
-    return allFiles;
-}) 
+function generate_callback(file, i, Objlen){
+  return function(err, stats){
+    console.log(file);
+    filesInfo['stat'][i] = stats['mtime'];
+    console.log(filesInfo);    
+    if(Objlen === i) 
+       workWithFiles();
+  }
+}
 
+function workWithFiles(){
+ for(var i in filesInfo["file"])
+ {
+    if(filesInfo['format'][i] != 'gz')
+    {
+     console.log('Работаем с файлом', filesInfo["name"][i]);
+      console.log('Есть ли архив у файла '+ filesInfo["name"][i] + "?");
+      var step1 = nameInGz(filesInfo["name"][i]);
+      if(step1 != false)
+        var step2 = dateGz(filesInfo["stat"][i], filesInfo["stat"][step1]);
+      if(step2 === true)
+        var step3 = delFiles(filesInfo["file"][step1]); //Зачем удалять, если он заменится?
+      if(step2 === true || step1 === false)
+        addInGz(filesInfo["file"][i]);
+    }  
+ } 
+//проверка на наличие архивов без исходного файла
+let flagok = 0;
+for(var i in filesInfo["file"])
+ {
+    if(filesInfo['format'][i] === 'gz')
+    {
+      for(var j in filesInfo["file"])
+      {
+        if(filesInfo['format'][j] != 'gz' && filesInfo['name'][j] === filesInfo['name'][i])
+            {
+              filesInfo['name'][j] = 'x'; //нашли пары файл - его архив
+              filesInfo['name'][i] = 'x'; //имена проверенных файлов заменили на "х"
+              flagok = 1;
+              break;
+            }
+      } 
+      
+    }  
+ } 
+
+for(var i in filesInfo["file"])
+  {
+    if(filesInfo['name'][i] != 'x' && flagok === 1)
+    {
+      console.log("Найден архив без исходного файла", filesInfo['file'][i]);
+      delFiles(filesInfo["file"][i]);
+    }  
+  }     
 
 }
 
-async function getFilesData(allItems){
-console.log('Hадо обработать следующие файлы', allItems);
-//for(var i=0; i< allItems.length; i++)
-//{
-var i=-1;
-{  
-  console.log(allItems.length);
- var i=i+1; 
- var nStr = allItems[i]; 
- new Promise((resolve, reject) =>{
-
-  fs.stat(__dirname + "/compres/" + allItems[i], function(error,stat){
-    if(error) {  return console.log(error);  } // если возникла ошибка 
-      return resolve(stat.mtime);
-  })})
-  .then(resolve =>  {
-    //console.log("resolve", resolve);
-    if(nStr.includes('gz'))
-      objArh[i] = {[allItems[i]]:'', [resolve]:''};
-    else
-      objFiles[i] = {[allItems[i]]:'',  [resolve]:''};
-  console.log('выводим считанные данные', resolve);   // выводим считанные данные 
-  console.log(objArh, objFiles);
-return objFiles;
-});
-}while(i >= allItems.length);
-//}
-
+function nameInGz(currentName){
+  for(var i in filesInfo["file"])
+ {
+    if(filesInfo['format'][i] === 'gz' && currentName === filesInfo['name'][i])
+      {
+      console.log('Архив существует');
+      return i;
+      }
+  } 
+ console.log("Для файла " + currentName + " архив не найден"); 
+ return false;
 }
 
+function dateGz(currentStat, step1Stat){
+ console.log('Сравниваю даты модификации файлов:', currentStat, " ",step1Stat);
+  if(currentStat > step1Stat)
+   {
+    console.log('Архивная версия устарела:');
+    return true;
+   }
+   else
+   {
+    console.log('Архивная версия ok');
+    return false;
+   }
+}
+
+function delFiles(nameFile){
+  try{
+   // var file = "/compres/" + nameFile;
+    fs.unlink(path + '/' + nameFile, (error) => {
+   // fs.unlink(__dirname + file, (error) => {
+      if(error) console.log("Ошибка на этапе удаления архива", error);
+    }); 
+    console.log('Устаревший архив ' + nameFile + ' удален.'); 
+  }
+  catch(err){
+    console.log("Возникла ошибка " + err + " на этапе удаления файла " + nameFile);
+  }
+}
+
+function addInGz(nameFile){
+try{
+  console.log('Новый архив создан для файла ', nameFile);
+ // var file = "/compres/" + nameFile;
+  //const readableStream = fs.createReadStream(__dirname + file); 
+ // const writeableStream = fs.createWriteStream(__dirname + file +".gz");
+  const readableStream = fs.createReadStream(path + '/' + nameFile); 
+  const writeableStream = fs.createWriteStream(path + '/' + nameFile +".gz");
+  const gzip = zlib.createGzip();
+  readableStream.pipe(gzip).pipe(writeableStream);
+}
+catch(err){
+  console.log("Возникла ошибка " + err + " на этапе создания архива " + nameFile);
+}
+}
