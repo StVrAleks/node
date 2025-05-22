00bac46acedbc2bbf2b2e12c16d60dcdd9a14576
StVrAleks <orchid86@yandex.ru>
2025-05-22 23:20:19 +0300

    Update app.js

--- app7_Uploader/app.js
+++ app7_Uploader/app.js
@@ -6,8 +6,8 @@ const multer  = require('multer');
 const jsonParser = express.json();
 const path = require('path');
 const fs = require("fs");
-const readline = require('readline-sync');
-const fetch = require('node-fetch');
+//const readline = require('readline-sync');
+//const fetch = require('node-fetch');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -39,59 +39,67 @@ app.get("/page", function (_, response) {
 
 app.post('/viewFiles', async function (request, response){
     try{
-         let allName = await getFilesName();
-       
-     console.log('2',allName);
-      let a = '20';
-      response.send(a);   
+      await fs.readdir(path.resolve(__dirname, "uploads"), (err, files) => {
+            if(err)
+            {
+                console.log('err', err);
+                response.send(); 
+            }    
+            let allFil = {};
+            for(var i=0; i<files.length; i++)
+                allFil[i] = files[i];
+            console.log('files', allFil);
+            response.send(allFil); 
+        });
     }
-    catch(err)
-    {console.log(err)};
+     catch(err){console.log(err)};
 });    
 
-async function getFilesName() {
- response = await fs.readdir(__dirname + "/uploads", (data, error) => {
-         if(error)
-            console.log(error);
-        
-    });
-    let data1 = await data.json();
-    let alldata = new Array;
-        for (const file of data1) 
-                alldata.push(file);   
-                 //   console.log('data',file);
-    console.log('data',alldata);
-    return alldata;
-            
-      //  let newData = data;
-       /*  for (let file of files)
-            newData.push(file);*/
-      //  console.log('В папке найдены файлы:', data);
-        // response.send(data); 
-       // response= data;
-       
-      
-     /*   let Alldata = "";
-        response.on("data", data => {
-            Alldata += data;
+app.post('/download', jsonParser, async function (request, response){
+try{
+   console.log(request.headers["accept"]);
+    let itemName = request.body.item;
+    console.log(request.body);
+      await fs.readdir(path.resolve(__dirname, "uploads"), (err, files) => {
+            if(err)
+            {
+                console.log('err', err);
+                response.send(); 
+            }    
+           console.log(files);
+            for(var i=0; i<files.length; i++)
+            { 
+                 console.log(itemName, files[i]);
+                if(itemName === files[i])
+                {
+                    console.log('tut1');
+                    
+                    response.setHeader('Content-disposition', 'attachment; filename=itemName');
+                   // fs.readFileSync(path.resolve(__dirname, "uploads",itemName)).pipe(response);}
+                    let data = fs.readFileSync(path.resolve(__dirname, "uploads",itemName));
+                        console.log(data);
+                        let sendF = JSON.parse(data);
+                        console.log(sendF);
+                       response.on('data', function(data) {
+                        console.log(data);
+                            let file = JSON.parse(data);
+                            response.send(file);
+                       })
+                   //https://developer.mozilla.org/ru/docs/Web/API/XMLHttpRequest_API/Sending_and_Receiving_Binary_Data     
+                    }
+                   // var clientData = JSON.parse(filestream); 
+                   // console.log('tytyt', path.resolve(__dirname, "uploads", itemName));
+                    //console.log();
+                    //response.send(clientData);
+                    //response.pipe(filestream);
+                    //filestream.pipe(response);
+            }
+         //   console.log("File not found");    
+           // console.log('files', allFil);
+        //    response.send(); 
         });
-        response.on("end", () => {
-            console.log('data',Alldata);
-            return Alldata;
-        });  */
-       // return newData;
-    
-}
-async function getFilesName() {
-    
-    try {
-        let data = await fs.readdir(__dirname + "/uploads");
-        console.log(data);
-        return data;
-    } catch (err) {
-        console.log("гетФиле",err);
     }
-    return;
-    
-}
+     catch(err){console.log(err)};
+});    
 app.listen(8181, ()=>console.log("Сервер запущен по адресу http://localhost:8181"));
+
