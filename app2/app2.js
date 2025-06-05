4380d32c52341e12b54d98efefba917157cb2c3c
StVrAleks <orchid86@yandex.ru>
2025-06-05 16:51:43 +0300

    Update app2.js

--- app2/app2.js
+++ app2/app2.js
@@ -1,4 +1,5 @@
 
+
 const http = require("http");
 const express = require("express"); // получаем модуль express
 // создаем приложение express
@@ -6,21 +7,44 @@ const app = express();
 //const jsonParser = express.json();
 //const urlencodedParser = express.urlencoded({extended: true});
 //app.use(express.urlencoded({ extended: true }));
+const urlencodedParser = express.urlencoded({extended: true});
+//const jsonParser = express.json();
+//app.use(express.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+  let myForm=`
+        <form action="/form" method='post'>
+           <span>Введите имя</span> 
+           </br>
+           <input type="text" name="newUser" value = '' > 
+           <span>Не менее 5 символов, включая заглавную</span>
+           </br>
+           </br>
+            <span>Введите возраст:</span> 
+            </br>
+            <input type="text" name="userAge" value = ''>
+            <span>Только цифры до 100</span>
+            </br>
+            <input type="submit" value='Отправить'>
+        </form>`;  
+	res.send(myForm);
+});
+
 try
 {
-app.get("/form", function (request, response) {
+app.post("/form", urlencodedParser, function (request, response) {
     let formErrorText = '</br><span style="color:black">Не менее 5 символов, включая заглавную</span></br>';
     let formErrorNumber = '</br><span>Только цифры до 100</span></br>'
-    let goodRez = '<h2 style="color:green">' + request.query.newUser + ', вы молодец!!! Данные заполнили верно!</h2>';
+  //  let goodRez = '<h2 style="color:green">' + request.query.newUser + ', вы молодец!!! Данные заполнили верно!</h2>';
     let nameUser = '';
     let age = '';
     let noEr = 10;
- 
-    if(request.url != "/form")
-       {
-         nameUser = request.query.newUser;
-         age = request.query.userAge;
-
+ console.log(request.url);
+   // if(request.url != "/form")
+     // {
+         nameUser = request.body.newUser;
+         age = request.body.userAge;
+console.log(nameUser, age);
          //флаг наличия ошибок
          noEr = 0;
         //исправляем ошибочно введенные пробелы
@@ -47,21 +71,36 @@ app.get("/form", function (request, response) {
             formErrorText = '</br><span style="color:red">Данные введены некорректно!</span></br>';
             noEr = 1;
         }
-        }
+        //}<form action="form?newUser=${nameUser}${request.url}">
     let myForm=`
-        <form action="/form">
+        <form action="form" method='POST'>
+            <span>Введите имя</span> 
             <input type="text" name="newUser" value = '${nameUser}' > 
             ${formErrorText}
+            <span>Введите возраст:</span> 
             <input type="text" name="userAge" value = '${age}'>
             ${formErrorNumber}
             <input type="submit" value='Отправить'>
         </form>`;
-
+console.log('1',request.query.name, '2', request.body.name,'3', nameUser);
         if(noEr === 0)
-            response.send(goodRez);
+            response.redirect(301, '/success?name='+nameUser)
+           // response.send(goodRez);
         else
+        {
+            response.statusCode = 200;
+            response.setHeader('Content-Type', 'text/html');
             response.send(myForm);
+        }    
     });
 }
-catch{console.log(error);}
+catch(error){
+    console.log(error);
+}
+
+app.get('/success', (request, response) => {
+    let goodRez = '<h2 style="color:green">' + request.query.name + ', вы молодец!!! Данные заполнили верно!</h2>';
+    response.send(goodRez);
+});
+
 app.listen(8181, ()=>console.log("Сервер запущен по адресу http://localhost:8181"));
